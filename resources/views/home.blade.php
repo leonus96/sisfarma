f56228fdd1f9fecc8ea5ba308a99834f25933426
Carlos Córdova <carlosab1802@gmail.com>
2019-04-07 14:13:26 -0500

    Quitando los comentarios de mrd.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,22 @@
 @extends('layouts.master')
 
 @section('content')
-    <!-- Content Header (Page header) -->
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1 class="m-0 text-dark">Vender</h1>
-                </div><!-- /.col -->
+                </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Ventas</a></li>
                         <li class="breadcrumb-item active">Vender</li>
                     </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
+                </div>
+            </div>
+        </div>
     </div>
-    <!-- /.content-header -->
 
-    <!-- Main content -->
     <section class="content">
         <div class="row">
             <div class="col-12">
@@ -112,7 +109,6 @@
             </div>
         </div>
     </section>
-    <!-- /.content -->
     <div class="modal fade" id="new_client_modal" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
